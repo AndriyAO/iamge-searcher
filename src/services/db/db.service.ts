97ed229f2dccca3052a8a867c90eee2c9ad654a8
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-19 11:23:27 +0200

    add DbService

---
+++ src/services/db/db.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { DataStore } from 'notarealdb';
+import { find } from 'lodash';
+import { Image, User } from 'src/interfaces';
+
+@Injectable()
+export class DbService {
+    private store;
+    constructor() {
+        this.store = new DataStore('./')
+    }
+
+    create(collection: string, data: User | Image | History) {
+        return this.store.collection(collection).create(data);
+    }
+
+    update(collection: string, data: User | Image | History) {
+        return this.store.collection(collection).update(data);
+    }
+
+    getById(collection: string, id: string) {
+        return this.store.collection(collection).get(id);
+    }
+
+    delete(collection: string, id: string) {
+        return this.store.collection(collection).delete(id);
+    }
+
+    getByKey(collection: string, key: string, value: string) {
+        const searchOption = {};
+        searchOption[key] = value;
+        return find(this.getAll(collection), searchOption);
+    }
+
+    getAll(collection) {
+        return this.store.collection(collection).list();
+    }
+}
