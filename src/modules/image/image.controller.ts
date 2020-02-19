0b0e791a3c26d3f8aa810c8debd7f1753837de20
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-19 14:50:03 +0200

    update Image: add like functionality

--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -1,24 +1,25 @@
-import { Controller, Get, Query, Post, Param, HttpException, NotAcceptableException } from '@nestjs/common';
+import { Controller, Get, Query, Post, Param, NotAcceptableException } from '@nestjs/common';
 import { ImageService } from './image.service';
 
 @Controller('image')
 export class ImageController {
     constructor(private imageService: ImageService) {}
 
-    @Get()
+    @Get(':id')
     getImages(
         @Query('query') query, 
         @Query('offset') offset,
         @Query('limit') limit,
+        @Param('id') userId,
     ) {
         if (limit > 100) {
             throw new NotAcceptableException('Limit more than 100 not allowed.');
         }
-        return this.imageService.searchImages({ query, offset, limit });
+        return this.imageService.searchImages({ query, offset, limit }, userId);
     }
 
-    @Post(':id')
-    likeImage(@Param('id') id) {
-        return this.imageService.likeImage(id);
+    @Post(':id/user/:userId')
+    likeImage(@Param('id') id, @Param('userId') userId) {
+        return this.imageService.likeImage(id, userId);
     }
 }
--- src/modules/image/image.service.ts
+++ src/modules/image/image.service.ts
@@ -1,37 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import * as giphyApi from'giphy-api';
-import { get } from 'lodash';
-import { SearchOptions, Image, ImageResponse } from '../../interfaces';
+import { get, some } from 'lodash';
+import { SearchOptions, Image, ImageResponse, Like } from '../../interfaces';
+import { DbService } from 'src/services/db/db.service';
 
 @Injectable()
 export class ImageService {
+    private readonly COLLECTION_NAME = 'likes';
+
     private client;
-    constructor() {
-        this.client = giphyApi('QPxKSYo3WJEJFqGf2pFnTKCQJ74ttDER');
+    constructor(private databaseService: DbService) {
+        this.client = giphyApi('QPxKSYo3WJEJFqGf2pFnTKCQJ74ttDER'); // move to env
      }
 
-    async searchImages(searchOptions: SearchOptions): Promise<ImageResponse> {
+    async searchImages(searchOptions: SearchOptions, userId: string): Promise<ImageResponse> {
         const response = await this.client.search({ q: searchOptions.query, ...searchOptions });
-        const images = this.transformImage(response);
+        const images = this.transformImage(response, userId);
         return {
             images,
             pagination: response.pagination,
         };
     }
 
-    likeImage(imageId: string) {
-        return `image ${imageId}`;
+    likeImage(imageId: string, userId: string): Like {
+        const likeId = this.databaseService.create(this.COLLECTION_NAME, { imageId, userId });
+        return this.databaseService.getById(this.COLLECTION_NAME, likeId);
     }
 
-    private transformImage(data: giphyApi.GIFObject): Image[] {
+    private transformImage(data: giphyApi.GIFObject, userId: string): Image[] {
         const images = get(data, 'data', []);
         return images.map(image => {
             return {
                 id: image.id,
                 type: image.type,
                 title: image.title,
-                url: get(image, 'images.original.url', null),            
+                url: this.getImageUrl(image),
+                liked: this.ifImageLiked(image, userId),
             }
         })
+    }
+
+    private getImageUrl(image: any): string {
+        return get(image, 'images.original.url', null);
+    }
+
+    private ifImageLiked(image: any, userId: string): boolean {
+        const likes = this.databaseService.getByKey(this.COLLECTION_NAME, 'userId', userId);
+        return some(likes, ['imageId', image.id]);
     }
 }
