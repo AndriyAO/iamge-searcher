47889254164e454108bb032b3aaacc052820cb43
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-19 12:42:32 +0200

    update Module: add history module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { ImageModule } from './modules/image/image.module';
 import { UserModule } from './modules/user/user.module';
 import { DbService } from './services/db/db.service';
+import { HistoryModule } from './modules/history/history.module';
 
 @Module({
-  imports: [ImageModule, UserModule],
+  imports: [ImageModule, UserModule, HistoryModule],
   controllers: [AppController],
   providers: [DbService],
 })
