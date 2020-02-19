ed264c5321e5a77624ccef6d5df6968031d20db0
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-19 11:24:27 +0200

    update (User): add login and work with db service

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,23 +1,26 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
-import { User } from 'src/interfaces/user.interface';
+import { Controller, Post, Body, Get, Param, Query, HttpException } from '@nestjs/common';
+import { User } from 'src/interfaces';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    
+    constructor(private userService: UserService) { }
 
     @Post()
     createUser(@Body() user: User) {
-        return ;
+        return this.userService.createUser(user);
     }
 
     @Get(':id')
     getUserById(@Param('id') id): User {
-        return;
+        return this.userService.getUserById(id);
     }
-
 
     @Post('login')
     login(@Body() user: User) {
-        return;
+        if (!user.email && !user.password) {
+            throw new HttpException('Email or password is missing', 400);
+        }
+        return this.userService.login(user);
     }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { DataStore } from 'notarealdb'
-import { User } from 'src/interfaces/user.interface';
+import { Injectable, HttpException, ForbiddenException, NotFoundException } from '@nestjs/common';
+import { User } from 'src/interfaces';
+import { DbService } from 'src/services/db/db.service';
 
 @Injectable()
 export class UserService {
+    constructor(private databaseServcie: DbService) { }
     createUser(user: User) {
-        
+        return this.databaseServcie.create('user', user);        
+    }
+
+    getUserById(id: string): User {
+        return this.databaseServcie.getById('user', id);
+    }
+
+    getUserByEmail(email: string): User {
+        return this.databaseServcie.getByKey('user', 'email', email);
+    }
+
+    login(data: User) {
+        const user = this.databaseServcie.getByKey('user', 'email', data.email);
+        console.log(user, data)
+        if (!user) {
+            throw new NotFoundException('User does not exist');
+        }
+        if (!this.comparePassword(data.password, user.password)) {
+            throw new ForbiddenException();
+        }
+        return user;         
+    }
+
+    private comparePassword(password: string, oldPassword: string) {
+        return password === oldPassword;
     }
 }
