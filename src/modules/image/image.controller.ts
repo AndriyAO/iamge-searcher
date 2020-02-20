b52c40a4d9e5b0c88e4bc8214c9bc46d892d044b
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-21 00:37:10 +0200

    update (Image): add jwt auth

--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -18,18 +18,17 @@ export class ImageController {
         if (limit > 100) {
             throw new NotAcceptableException('Limit more than 100 not allowed.');
         }
-        const userId = get(req, 'user.id', null);
-        return this.imageService.searchImages(
-            { 
-                q: query, 
-                offset: offset || 0, 
-                limit: limit || 15
-            }, 
-            userId);
+        const searchOption = { 
+            q: query, 
+            offset: offset || 0, 
+            limit: limit || 15
+        };
+        return this.imageService.searchImages(searchOption, req.user.userId);
     }
 
-    @Post(':id/user/:userId')
-    likeImage(@Param('id') id, @Param('userId') userId) {
-        return this.imageService.likeImage(id, userId);
+    @UseGuards(JwtAuthGuard)
+    @Post(':id/')
+    likeImage(@Request() req, @Param('id') id) {
+        return this.imageService.likeImage(id, req.user.userId);
     }
 }
