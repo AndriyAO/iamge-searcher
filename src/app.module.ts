1fe09765bbf668e1d1a2028da11bd0fb16c19068
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-20 14:28:52 +0200

    update App: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { ImageModule } from './modules/image/image.module';
 import { UserModule } from './modules/user/user.module';
 import { DbService } from './services/db/db.service';
 import { HistoryModule } from './modules/history/history.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ImageModule, UserModule, HistoryModule],
+  imports: [
+    AuthModule,
+    UserModule, 
+    ImageModule,
+    HistoryModule, 
+  ],
   controllers: [AppController],
   providers: [DbService],
+  exports: [UserModule]
 })
 export class AppModule {}
