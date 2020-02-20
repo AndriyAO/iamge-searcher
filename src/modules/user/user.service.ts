72b7d0b83c262d78b0edf9002a1182e13d8f745e
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-20 14:35:29 +0200

    update User: rename

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,35 +1,40 @@
 import { Injectable, NotAcceptableException, NotFoundException, ForbiddenException } from '@nestjs/common';
 import { User } from 'src/interfaces';
 import { DbService } from '../../services/db/db.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class UserService {
     private readonly COLLECTION_NAME = 'user';
 
-    constructor(private databaseServcie: DbService) { }
+    constructor(
+        private databaseService: DbService,
+        private authService: AuthService,
+        ) { }
     
-    createUser(user: User) {
-        if (this.checkIfUserExist(user)) {
+    createUser(data: User) {
+        if (this.checkIfUserExist(data)) {
             throw new NotAcceptableException('User already exist');
         }
-        const userId = this.databaseServcie.create(this.COLLECTION_NAME, user);
-        return this.databaseServcie.getById(this.COLLECTION_NAME, userId);        
+        const userId = this.databaseService.create(this.COLLECTION_NAME, data);
+        const user = this.databaseService.getById(this.COLLECTION_NAME, userId);
+        return this.databaseService.getById(this.COLLECTION_NAME, userId);        
     }
 
     getUserById(id: string): User {
-        return this.databaseServcie.getById(this.COLLECTION_NAME, id);
+        return this.databaseService.getById(this.COLLECTION_NAME, id);
     }
 
     getUserByEmail(email: string): User {
-        return this.databaseServcie.getByKey(this.COLLECTION_NAME, 'email', email)[0];
+        return this.databaseService.getByKey(this.COLLECTION_NAME, 'email', email)[0];
     }
 
     getUserByUsername(username: string): User {
-        return this.databaseServcie.getByKey(this.COLLECTION_NAME, 'username', username)[0];
+        return this.databaseService.getByKey(this.COLLECTION_NAME, 'username', username)[0];
     }
 
     login(data: User) {
-        const user = this.databaseServcie.getByKey(this.COLLECTION_NAME, 'email', data.email)[0];
+        const user = this.databaseService.getByKey(this.COLLECTION_NAME, 'email', data.email)[0];
         if (!user) {
             throw new NotFoundException('User does not exist');
         }
@@ -40,8 +45,8 @@ export class UserService {
     }
 
     private checkIfUserExist(user: User) {
-        return !!this.databaseServcie.getByKey(this.COLLECTION_NAME, 'email', user.email)[0] && 
-        !!this.databaseServcie.getByKey(this.COLLECTION_NAME, 'username', user.username)[0];
+        return !!this.databaseService.getByKey(this.COLLECTION_NAME, 'email', user.email)[0] && 
+        !!this.databaseService.getByKey(this.COLLECTION_NAME, 'username', user.username)[0];
     }
 
     private comparePassword(password: string, oldPassword: string) {
