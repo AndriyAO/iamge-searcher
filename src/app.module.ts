19347589e218a39d17aa41be3dd90aed415da4e1
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-20 17:29:21 +0200

    update (AppModule): add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { ImageModule } from './modules/image/image.module';
 import { UserModule } from './modules/user/user.module';
@@ -8,6 +9,7 @@ import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     AuthModule,
     UserModule, 
     ImageModule,
