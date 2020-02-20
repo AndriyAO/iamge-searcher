993dd5d1f8c197b386294b598dd3dd698eb54d6b
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-20 17:32:06 +0200

    update Auth: fix bug

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, forwardRef, Inject } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
+    @Inject(forwardRef(() => UserService))
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
   ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.userService.getUserByEmail(username);
+    const user = await this.userService.getUserByUsername(username);
     if (user && user.password === pass) {
       const { password, ...result } = user;
       return result;
@@ -18,8 +19,8 @@ export class AuthService {
     return null;
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+  login(user: any) {
+    const payload = { username: user.username, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
