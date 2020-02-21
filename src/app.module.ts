52818f34ff21527152601aeef334fe1b0fadb4aa
Andrii Antoniuk <andrii.antoniuk@bytebrand.net>
2020-02-21 13:16:30 +0200

    update (AppModule): rm import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { ImageModule } from './modules/image/image.module';
 import { UserModule } from './modules/user/user.module';
-import { DbService } from './services/db/db.service';
 import { HistoryModule } from './modules/history/history.module';
 import { AuthModule } from './modules/auth/auth.module';
 
@@ -16,7 +15,5 @@ import { AuthModule } from './modules/auth/auth.module';
     HistoryModule, 
   ],
   controllers: [AppController],
-  providers: [DbService],
-  exports: [UserModule]
 })
 export class AppModule {}
